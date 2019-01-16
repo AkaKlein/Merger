eddb3ce420cd656f24ce9e4cb3f5acbd73e8ee06
Andrea Klein <andrea.dana.klein@gmail.com>
2019-01-17 00:27:53 +0100

    Fix size of model result

--- GUI/model_result.cpp
+++ GUI/model_result.cpp
@@ -2,6 +2,7 @@
 
 #include <cmath>
 
+#include <QApplication>
 #include <QHBoxLayout>
 #include <QHeaderView>
 #include <QTableWidgetItem>
@@ -18,51 +19,54 @@ ModelResult::ModelResult(QWidget* parent, std::unique_ptr<ModelInterface> model,
     ColumnVector prices = model->ComputePrices();
     ColumnVector quantities = model->ComputeQuantities(prices);
 
-    for (int i = 0; i < 5; ++i)
-    {
-        ColumnVector v;
+    PopulateRow(0, tr("Prices"), prices);
+    PopulateRow(1, tr("Quantities"), quantities);
+    PopulateRow(2, tr("Costs"), model->ComputeCosts(quantities));
+    PopulateRow(3, tr("Profits"), model->ComputeProfits(prices));
+    PopulateRow(4, tr("Consumer Welfare"), model->ComputeConsumerWelfare(prices));
 
-        if (i == 0)
-        {
-            m_table_widget->setVerticalHeaderItem(0, new QTableWidgetItem(tr("Prices")));
-            v = prices;
-        }
-        else if (i == 1)
-        {
-            m_table_widget->setVerticalHeaderItem(1, new QTableWidgetItem(tr("Quantities")));
-            v = quantities;
-        }
-        else if (i == 2)
-        {
-            m_table_widget->setVerticalHeaderItem(2, new QTableWidgetItem(tr("Costs")));
-            v = model->ComputeCosts(quantities);
-        }
-        else if (i == 3)
-        {
-            m_table_widget->setVerticalHeaderItem(3, new QTableWidgetItem(tr("Profits")));
-            v = model->ComputeProfits(prices);
-        }
-        else
-        {
-            m_table_widget->setVerticalHeaderItem(4, new QTableWidgetItem(tr("Consumer Welfare")));
-            v = model->ComputeConsumerWelfare(prices);
-        }
-
-        for (int j = 0; j < model->GetNumberOfProducts(); ++j)
-        {
-            QTableWidgetItem *item = new QTableWidgetItem(tr("%1").arg(v[j]));
-            m_table_widget->setItem(i, j, item);
-        }
-    }
-
-    for (int i = 0; i < 3; ++i)
+    for (int i = 0; i < model->GetNumberOfProducts(); ++i)
         m_table_widget->setHorizontalHeaderItem(i, new QTableWidgetItem(tr("Product %1").arg(i + 1)));
 
     m_table_widget->horizontalHeader()->setStretchLastSection(true);
     m_table_widget->verticalHeader()->setStretchLastSection(true);
 
+
     QHBoxLayout* layout = new QHBoxLayout;
     layout->addWidget(m_table_widget);
 
     setLayout(layout);
+
+    QApplication::processEvents();
+    AdjustTableSize();
+}
+
+void ModelResult::PopulateRow(int index, QString const& name, ColumnVector const& values)
+{
+    m_table_widget->setVerticalHeaderItem(index, new QTableWidgetItem(name));
+    for (int i = 0; i < m_table_widget->rowCount(); ++i)
+    {
+        QTableWidgetItem *item = new QTableWidgetItem(tr("%1").arg(values[i]));
+        m_table_widget->setItem(index, i, item);
+    }
+}
+
+void ModelResult::AdjustTableSize()
+{
+    m_table_widget->resizeColumnsToContents();
+    m_table_widget->resizeRowsToContents();
+
+    QRect rect = m_table_widget->geometry();
+    
+    int width = 30 + m_table_widget->verticalHeader()->width();
+    for (int i = 0; i < m_table_widget->columnCount(); ++i)
+        width += m_table_widget->columnWidth(i);
+    rect.setWidth(width);
+
+    int height = 10 + m_table_widget->horizontalHeader()->height();
+    for (int i = 0; i < m_table_widget->rowCount(); ++i)
+        height += m_table_widget->rowHeight(i);
+    rect.setHeight(height);
+
+    m_table_widget->setMinimumSize(std::min(width, 1000), height);
 }
